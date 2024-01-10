61a81854dd8addc27c76ef2ebe111911b4a8fa0e
Kirti Bijay Padhi <kirtibijaypadhi.12@gmail.com>
2024-01-10 22:00:01 +0530

    changes made to solve solar

--- backend/homework/3/src/main/java/org/example/question1/Billing.java
+++ backend/homework/3/src/main/java/org/example/question1/Billing.java
@@ -1,27 +1,46 @@
+/**
+ * Question - 2 - part
+ */
+
 package org.example.question1;
 
-import java.util.logging.Logger;
+class Billing {
 
+    public static double[] computePaymentAmount(Patient patient, double amount) {
+        double[] payments = new double[2];
 
-public class HMSMain {
-    private static final Logger logger = Logger.getLogger(HMSMain.class.getName());
+        HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
 
-    public static void main(String[] a) {
-        User staff = new User();
-        InsuranceBrand insuranceBrand = new BlueCrossBlueShield();
-        HealthInsurancePlan insurancePlan = new PlatinumPlan();
-        insurancePlan.setOfferedBy(insuranceBrand);
-        staff.setInsurancePlan(insurancePlan);
+        // your logic
+        if(patientInsurancePlan == null)
+        {
+            double discount = 20;
+            payments[0] = 0;
+            payments[1] = amount - discount;
+            return payments;
+        }
+        double coverage = patientInsurancePlan.getCoverage();
+        double insuranceAmount = amount * coverage;
+        payments[0] = insuranceAmount;
+        if(patientInsurancePlan instanceof PlatinumPlan)
+        {
+            payments[1] = amount - insuranceAmount - ((PlatinumPlan) patientInsurancePlan).getDiscount();
+        }
+        else if(patientInsurancePlan instanceof GoldPlan)
+        {
+            payments[1] = (amount - insuranceAmount - ((GoldPlan) patientInsurancePlan).getDiscount());
+        }
+        else if(patientInsurancePlan instanceof SilverPlan)
+        {
+            payments[1] = (amount*(1-coverage) - ((SilverPlan) patientInsurancePlan).getDiscount());
+        }
+        else if (patientInsurancePlan instanceof BronzePlan) {
+            payments[1] = amount*(1-coverage) - ((BronzePlan) patientInsurancePlan).getDiscount();
+        }
 
-        double salary = 5000;
-        int age = 56;
-        boolean isSmoking = true;
-
-        double premium = insurancePlan.compMonthlyPrem(salary, age, isSmoking);
-        String premiumMessage = String.format("The monthly premium for a salary of $%.2f, age %d, smoking: %b is -> $%.2f",
-                salary, age, isSmoking, premium);
-
-        logger.info(premiumMessage);
+        return payments;
     }
+
 }
+
 
