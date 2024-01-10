624f9de75c526f2eec1835a6603c4c3ff190b472
Kirti Bijay Padhi <kirtibijaypadhi.12@gmail.com>
2024-01-10 20:56:36 +0530

    made changes in BlueCross to reduce code smells

--- backend/homework/3/src/main/java/org/example/question1/BlueCrossBlueShield.java
+++ backend/homework/3/src/main/java/org/example/question1/BlueCrossBlueShield.java
@@ -1,53 +1,81 @@
 package org.example.question1;
 
-public class BlueCrossBlueShield implements InsuranceBrand{
+
+
+
+public class BlueCrossBlueShield implements InsuranceBrand {
     @Override
     public double compMonthlyPrem(HealthInsurancePlan insurancePlan, int age, boolean smoking) {
         double insurance = 0;
-        if(insurancePlan instanceof PlatinumPlan)
-        {
-            if(age > 55)
-            {
-                insurance += 200;
-            }
-            if(smoking)
-            {
-                insurance += 100;
-            }
+
+        if (insurancePlan instanceof PlatinumPlan) {
+            insurance += calculatePlatinumPremium(age, smoking);
+        } else if (insurancePlan instanceof GoldPlan) {
+            insurance += calculateGoldPremium(age, smoking);
+        } else if (insurancePlan instanceof SilverPlan) {
+            insurance += calculateSilverPremium(age, smoking);
+        } else if (insurancePlan instanceof BronzePlan) {
+            insurance += calculateBronzePremium(age, smoking);
         }
-        else if(insurancePlan instanceof GoldPlan)
-        {
-            if(age > 55)
-            {
-                insurance += 150;
-            }
-            if(smoking)
-            {
-                insurance += 90;
-            }
-        }
-        else if(insurancePlan instanceof SilverPlan)
-        {
-            if(age > 55)
-            {
-                insurance += 100;
-            }
-            if(smoking)
-            {
-                insurance += 80;
-            }
-        }
-        else if(insurancePlan instanceof BronzePlan)
-        {
-            if(age > 55)
-            {
-                insurance += 50;
-            }
-            if(smoking)
-            {
-                insurance += 70;
-            }
-        }
+
+        // Uncovered code
+
         return insurance;
+    }
+
+    private double calculatePlatinumPremium(int age, boolean smoking) {
+        double premium = 0;
+
+        if (age > 55) {
+            premium += 200;
+        }
+
+        if (smoking) {
+            premium += 100;
+        }
+
+        return premium;
+    }
+
+    private double calculateGoldPremium(int age, boolean smoking) {
+        double premium = 0;
+
+        if (age > 55) {
+            premium += 150;
+        }
+
+        if (smoking) {
+            premium += 90;
+        }
+
+        return premium;
+    }
+
+    private double calculateSilverPremium(int age, boolean smoking) {
+        double premium = 0;
+
+        if (age > 55) {
+            premium += 100;
+        }
+
+        if (smoking) {
+            premium += 80;
+        }
+
+        return premium;
+    }
+
+    private double calculateBronzePremium(int age, boolean smoking) {
+        double premium = 0;
+
+        if (age > 55) {
+            premium += 50;
+        }
+
+        if (smoking) {
+            premium += 70;
+        }
+
+        return premium;
     }
 }
