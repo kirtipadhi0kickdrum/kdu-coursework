da572d601cf3dfd8e4d2e7a2ca30c3353407c77b
Kirti Bijay Padhi <kirtibijaypadhi.12@gmail.com>
2024-01-22 21:51:04 +0530

    Spring handsin - 1 submit

---
+++ backend/homework/6/src/main/java/org/example/tyre/Tyre.java
@@ -0,0 +1,22 @@
+package org.example.tyre;
+
+public class Tyre {
+    private String brand;
+    private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
