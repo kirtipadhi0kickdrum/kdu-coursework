44311c45085d701d875b4b30780a714a34e99da6
Kirti Bijay Padhi <kirtibijaypadhi.12@gmail.com>
2024-02-09 13:35:03 +0530

    add assessment files

---
+++ frontend/assessments/1/script.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+}
